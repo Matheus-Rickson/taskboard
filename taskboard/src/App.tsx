c5fbfb89ead9c7b886c4e8510132a2e7f3b30434
Matheus Rickson Gomes <Contato.rickson@outlook.com>
2021-10-06 18:01:57 -0300

    version1

---
+++ taskboard/src/App.tsx
@@ -0,0 +1,12 @@
+import * as component from './App.styles'; // Asteristico server pra importar tudo de uma lib;
+
+const App = () => {
+  return (
+    <component.container>
+
+
+    </component.container>
+
+  );
+}
+export default App;
